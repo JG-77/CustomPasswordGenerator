046f2485fb292ac756d6766f74ecc2c34e30ef12
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-03-23 18:53:54 -0700

    Added a couple html comments for future editing. Created 4 variables in JS file that will represent character selection.

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  prompt("How many characters do you generated password to be?(Must be between 8 and 128 characters)");
+  var password = generatePassword(addNumber,addSpecial,addUpper,addLower);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
+//booleans for 'confirm' messages
+var addNumber = true
+var addSpecial = true
+var addUpper = true
+var addLower = true
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
