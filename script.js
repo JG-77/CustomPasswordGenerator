546299a8e68dc664b71a24e7f7f0167a757e65d6
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-03-23 20:16:28 -0700

    Added window.prompt with assigned variable but value is not read. 'If' statement added to end function if cancelled.

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  prompt("How many characters do you generated password to be?(Must be between 8 and 128 characters)");
+  var pwlength = window.prompt("How many characters do you generated password to be?(Must be between 8 and 128 characters)");
   var password = generatePassword(addNumber,addSpecial,addUpper,addLower);
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+  
+  if (!pwLength) {
+      return;
+  }
 }
 
-//booleans for 'confirm' messages
+
+//booleans for future confirm() messages
 var addNumber = true
 var addSpecial = true
 var addUpper = true
