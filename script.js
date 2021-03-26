6988f7c8d0143ae1f1ce756d4bef92a084cf7a19
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-03-25 18:50:01 -0700

    Array of special characters added to addSpecialOpt variable. However, four special characters gave the array code errors.

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 //variables for character type 
 var addNumberOpt = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var addSpecialOpt = []
+//double quote, single quote, backslash, and grave accent unable to be used in special character string
+var addSpecialOpt = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "{", "|", "}", "~"]; 
 var addUpperOpt = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var addLowerOpt = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
