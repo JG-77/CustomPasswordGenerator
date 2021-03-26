451c1ad8e38650452a179c30611dd9696378f49a
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-03-25 19:02:08 -0700

    Reorganized js file structure to make 'writePassword' and'generatePassword' into two different functions. All additional logic will be in 'generatepassword' function.

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ var generateBtn = document.querySelector("#generate");
 //variables for character type 
 var addNumberOpt = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 //double quote, single quote, backslash, and grave accent unable to be used in special character string
-var addSpecialOpt = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "{", "|", "}", "~"]; 
+var addSpecialOpt = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"]; 
 var addUpperOpt = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var addLowerOpt = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Write password to the #password input
 function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+    passwordText.value = password;
+}
+
+function generatePassword(){
+    
     var pwLength = window.prompt("How many characters do you generated password to be?(Must be between 8 and 128 characters)");
     if (!pwLength) {
         return;
@@ -23,10 +31,6 @@ function writePassword() {
     var addUpper = window.confirm("Would you like password to generate uppercase letters?");
     var addLower = window.confirm("Would you like password to generate lowercase letters?");
     
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    
-    passwordText.value = password;
     
     //If user clicks 'cancel,' selection is not added
     if (!addNumber) {
@@ -41,11 +45,8 @@ function writePassword() {
     if (!addLower) {
         return;
     }
-}
+    }
 
-function generatePassword(){
-    return "placeholder"
-}
 
 
 
