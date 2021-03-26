78984728f5afe325d2db3db24008356c6be06f9d
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-03-25 18:32:06 -0700

    Arrays added for lowercase, uppercase, and number variables.

--- script.js
+++ script.js
@@ -2,14 +2,12 @@
 var generateBtn = document.querySelector("#generate");
 
 //variables for character type 
-var addNumberOpt = ""
-var addSpecialOpt = ""
-var addUpperOpt = ""
-var addLowerOpt = ""
+var addNumberOpt = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var addSpecialOpt = []
+var addUpperOpt = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var addLowerOpt = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Write password to the #password input
-
-
 function writePassword() {
     var pwLength = window.prompt("How many characters do you generated password to be?(Must be between 8 and 128 characters)");
     if (!pwLength) {
